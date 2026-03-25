0b064b9a078e25c9b93c95b5b87a49c90c88dcc1
tanks8015-hue <tanks8015@gmail.com>
2026-03-25 11:30:51 +0400

    SQL запрос тест данные для таблиц

--- SQL-запросы/тест_данные.sql
+++ SQL-запросы/тест_данные.sql
@@ -5,6 +5,6 @@ GO
 INSERT INTO Categories (CategoryName) VALUES (N'Документы'), (N'Медиа'), (N'Архивы');
 GO
 
--- Добавляем пару тестовых пользователей
+-- Добавляем пару тестовых пользователей (тест)
 INSERT INTO Users (UserName) VALUES (N'Иван Иванов'), (N'Алексей Попов');
 GO
