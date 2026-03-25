bbe5d578cf66f162c769f716f87bb24cf24e996e
tanks8015-hue <tanks8015@gmail.com>
2026-03-25 10:03:37 +0400

    Группа Б: добавлена валидация имени файла

--- Alenkin_Zadanie_3/ResourceManager.h
+++ Alenkin_Zadanie_3/ResourceManager.h
@@ -3,10 +3,15 @@
 #include <sqlext.h>
 #include <string>
 #include <iostream>
+#include <regex> // Нужно для проверки спецсимволов
 
 class ResourceManager {
 private:
     SQLHDBC hDbc;
+
+    // Вот эта строка потерялась! Объявляем функцию валидации имени
+    bool IsValidName(const std::wstring& name);
+
 public:
     ResourceManager(SQLHDBC connectionHandle);
     bool AddResource(const std::wstring& name, SQLINTEGER size, SQLINTEGER categoryId, SQLINTEGER ownerId);
